3e7ea382d4ec295b874bde46d297032df549f660
Sveta <lazarevasveta1993@gmail.com>
2019-11-30 17:58:08 +0200

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/new/', views.post_new, name='post_new'),
 ]
